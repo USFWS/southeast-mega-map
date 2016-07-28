fff3ce4a3117a02985afdd4445d490a269814d8a
Roy Hewitt <roy_hewitt@fws.gov>
2016-07-28 13:33:15 -0400

    Merge util and domUtil

--- src/js/util.js
+++ src/js/util.js
@@ -1,14 +1,54 @@
 (function () {
   'use strict';
 
+  require('classlist-polyfill');
+
   var _ = {
     defaults: require('lodash.defaults'),
     find: require('lodash.find'),
-    string: require('underscore.string')
+    string: require('underscore.string'),
+    create: create,
+    remove: remove,
+    addClass: addClass,
+    addClasses: addClasses,
+    removeClass: removeClass,
+    toggleClass: toggleClass
   };
 
-  module.exports = {
-    _: _
-  };
+  function create(tagName, className, container) {
+    var el = document.createElement(tagName);
+
+    if (typeof className === 'string') el.className = className;
+    else if (typeof className === 'object') addClasses(el, className);
+
+    if (container) container.appendChild(el);
+
+    return el;
+  }
+
+  function remove(el) {
+    var parent = el.parentNode;
+    if (parent) parent.removeChild(el);
+  }
+
+  function addClass(el, name) {
+    el.classList.add(name);
+  }
+
+  function addClasses(el, classes) {
+    classes.forEach(function (name) {
+      el.classList.add(name);
+    });
+  }
+
+  function removeClass(el, name) {
+    el.classList.remove(name);
+  }
+
+  function toggleClass(el, name) {
+    el.classList.toggle(name);
+  }
+
+  module.exports = _;
 
 })();
