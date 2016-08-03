9b0254c6280ee6f0c25f1f28fa92da71d1090ef9
Roy Hewitt <roy_hewitt@fws.gov>
2016-08-03 09:39:18 -0400

    Create a module to handle text-only view

---
+++ src/js/office-list.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+
+  var emitter = require('./mediator');
+
+  function init() {
+
+  }
+
+  function registerHandlers() {
+    emitter.on('', render);
+  }
+
+  function render(offices) {
+
+  }
+
+  module.exports.init = init;
+})();
