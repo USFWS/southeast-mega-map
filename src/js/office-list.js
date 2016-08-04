88b64761b2da1b0ed28627efe96f54811e9f473d
Roy Hewitt <roy_hewitt@fws.gov>
2016-08-04 09:26:21 -0400

    Add icon property to offices so we can display an image with the text view

--- src/js/office-list.js
+++ src/js/office-list.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var emitter = require('./mediator');
+  var _ = require('./util');
   var template = require('../templates/li.jade');
   var list = document.querySelector('.office-list');
 
@@ -22,9 +23,28 @@
   function emptyHandler(offices) {
     render(offices);
   }
-
   function render(offices) {
+    offices = _.map(offices, function (office) {
+      office.properties.icon = getIconPath(office.properties);
+      return office;
+    });
     list.innerHTML = template({ offices: offices });
+  }
+
+  function getIconPath(office) {
+    var path = ['./svg/'];
+    switch (office.type) {
+      case 'National Wildlife Refuge':
+        path.push('blue-goose.svg');
+        break;
+      case 'National Fish Hatchery':
+        path.push('fisheries.svg');
+        break;
+      default:
+        path.push('building.svg');
+        break;
+     }
+     return path.join('');
   }
 
   module.exports.init = init;
