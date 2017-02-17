4846e76663735c5318058ada425c268340b39c45
Roy Hewitt <roy_hewitt@fws.gov>
2017-02-17 09:23:41 -0500

    Convert to ES6 syntax, use constructor pattern

--- src/js/offices.js
+++ src/js/offices.js
@@ -1,55 +1,74 @@
-(function () {
-  'use strict';
+const L = require('leaflet');
+const lunr = require('lunr');
+const objectAssign = require('object-assign');
+const emitter = require('./mediator');
+const _ = require('./util');
 
-  var xhr = require('xhr');
-  var L = require('leaflet');
-  var emitter = require('./mediator');
-  var _ = require('./util');
+let offices;
 
-  var offices;
+const OfficeService = function(data) {
+  if (!data) return new Error('You must provide a geojson object.');
+  this.offices = data;
+  this.bounds = L.geoJson(data).getBounds();
+  this.index = lunr(function() {
+    this.field('name', { boost: 10 });
+    this.field('type');
+    this.field('program');
+    this.field('city');
+    this.field('state');
+    this.field('zip');
+    this.field('narrative', { boost: 5 });
+    this.ref('href');
+  });
 
-  function init(cb) {
-    xhr.get('./data/offices.json', function (err, res) {
-      if (err) return cb(err);
-      if (res.statusCode !== 200) return cb(new Error('Could not download offices.'));
-      offices = JSON.parse(res.body);
-      return cb(null, offices);
-    });
-  }
+  const index = this.index;
 
-  function getBounds() {
-    return L.geoJson(offices).getBounds();
-  }
+  this.offices.features.forEach(function(office, i) {
+    const theOffice = objectAssign({ href: i }, office.properties);
+    index.add(theOffice);
+  });
+}
 
-  function getOffices() {
-    return offices;
-  }
+module.exports = OfficeService;
 
-  function getOffice(officeName) {
-    return _.find(offices.features, function (office) {
-      var name = normalizeOfficeName(office.properties.name);
-      return name.toLowerCase() === officeName.toLowerCase();
-    });
-  }
+OfficeService.prototype.getBounds = function() {
+  return this.bounds;
+}
 
-  // Remove special characters from office name
-  function normalizeOfficeName(officeName) {
-    return officeName.replace(/[^a-zA-Z ]+/g, '');
-  }
+OfficeService.prototype.getOffices = function() {
+  return this.offices;
+}
 
-  function getRandomOffice() {
-    var list = offices.features;
-    var randomOffice = list[Math.floor(Math.random()*list.length)];
-    // ToDo: Functionality to call this function recursively if the random
-    //       office is not open to the public?  Needs to be added to dataset.
-    emitter.emit('offices:random', randomOffice);
-  }
+OfficeService.prototype.search = function(query) {
+  const self = this;
+  return this.index
+    .search(query)
+    .sort(sortByScore)
+    .map(result => self.offices.features[result.ref]);
+}
 
-  exports.getOffices = getOffices;
-  exports.getOffice = getOffice;
-  exports.getRandomOffice = getRandomOffice;
-  exports.normalizeOfficeName = normalizeOfficeName;
-  exports.getBounds = getBounds;
-  exports.init = init;
+OfficeService.prototype.getOffice = function(officeName) {
+  return offices.features.find(office => {
+    const name = normalizeOfficeName(office.properties.name);
+    return name.toLowerCase() === officeName.toLowerCase();
+  });
+}
 
-})();
+// Remove special characters from office name
+OfficeService.prototype.normalizeOfficeName = function(officeName) {
+  return officeName.replace(/[^a-zA-Z ]+/g, '');
+}
+
+OfficeService.prototype.getRandomOffice = function() {
+  const list = offices.features;
+  const randomOffice = list[Math.floor(Math.random()*list.length)];
+  // ToDo: Functionality to call this function recursively if the random
+  //       office is not open to the public?  Needs to be added to dataset.
+  emitter.emit('offices:random', randomOffice);
+}
+
+function sortByScore(a, b) {
+  if (a.score > b.score) return -1;
+  if (a.score < b.score) return 1;
+  return 0;
+}
