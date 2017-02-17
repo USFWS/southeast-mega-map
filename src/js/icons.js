3036a60fff5cbc9640218b65c4eccfc5a269e409
Roy Hewitt <roy_hewitt@fws.gov>
2017-02-17 09:16:14 -0500

    Convert to ES6 syntac

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,29 +1,25 @@
-(function () {
-  'use strict';
+const L = require('leaflet');
 
-  var L = require('leaflet');
+const blueGoose = L.icon({
+  iconUrl: './svg/blue-goose.svg',
+  iconSize: [70, 90],
+  popupAnchor: [5, -17]
+});
 
-  var blueGoose = L.icon({
-    iconUrl: './svg/blue-goose.svg',
-    iconSize: [70, 90],
-    popupAnchor: [5, -17]
-  });
+const office = L.icon({
+  iconUrl: './svg/building.svg',
+  iconSize: [70, 50],
+  popupAnchor: [0, -20]
+});
 
-  var office = L.icon({
-    iconUrl: './svg/building.svg',
-    iconSize: [70, 50],
-    popupAnchor: [0, -20]
-  });
+const fisheries = L.icon({
+  iconUrl: './svg/fisheries.svg',
+  iconSize: [70, 50],
+  popupAnchor: [7, -25]
+});
 
-  var fisheries = L.icon({
-    iconUrl: './svg/fisheries.svg',
-    iconSize: [70, 50],
-    popupAnchor: [7, -25]
-  });
-
-  module.exports = {
-    blueGoose: blueGoose,
-    office: office,
-    fisheries: fisheries
-  };
-})();
+module.exports = {
+  blueGoose,
+  office,
+  fisheries
+};
