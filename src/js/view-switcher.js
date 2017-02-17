5a7d28873654750eb61b211477d5b3774f8f4cc0
Roy Hewitt <roy_hewitt@fws.gov>
2017-02-17 09:24:18 -0500

    Convert to ES6 syntax, use constructor pattern

--- src/js/view-switcher.js
+++ src/js/view-switcher.js
@@ -1,48 +1,38 @@
-(function () {
-  'use strict';
+const emitter = require('./mediator');
+const _ = require('./util');
 
-  var emitter = require('./mediator');
-  var _ = require('./util');
+const Switcher = function(data) {
+  this.view = data.view;
+  this.textViewContainer = data.textViewContainer;
+  this.mapViewContainer = data.mapViewContainer;
 
-  var textView = document.querySelector('.office-list');
-  var mapView = document.querySelector('#map');
+  if (this.view === 'text') this.showText();
 
-  var opts = {};
-  var currentView;
+  this.container = _.create('div', 'layer-switcher', document.body);
+  this.textButton = _.create('button', 'layer-switcher-button text-view-button', this.container);
+  this.mapButton = _.create('button', 'layer-switcher-button map-view-button', this.container);
+  this.textButton.innerHTML = 'List View';
+  this.mapButton.innerHTML = 'Map View';
 
-  function init() {
-    getCurrentView();
-    opts.container = _.create('div', 'layer-switcher', document.body);
-    opts.textButton = _.create('button', 'layer-switcher-button text-view-button', opts.container);
-    opts.mapButton = _.create('button', 'layer-switcher-button map-view-button', opts.container);
-    opts.textButton.innerHTML = 'List View';
-    opts.mapButton.innerHTML = 'Map View';
+  this.textButton.addEventListener('click', this.showText.bind(this));
+  this.mapButton.addEventListener('click', this.showMap.bind(this));
+};
 
-    opts.textButton.addEventListener('click', showText);
-    opts.mapButton.addEventListener('click', showMap);
-  }
+module.exports = Switcher;
 
-  function showText() {
-    _.addClass(textView, 'active');
-    currentView = 'text';
-    emitter.emit('view:changed', 'text');
-  }
+Switcher.prototype.showText = function() {
+  this.textViewContainer.classList.add('active');
+  this.view = 'text';
+  emitter.emit('view:changed', 'text');
+}
 
-  function showMap() {
-    _.removeClass(textView, 'active');
-    currentView = 'map';
-    emitter.emit('view:changed', 'map');
-  }
+Switcher.prototype.showMap = function() {
+  this.textViewContainer.classList.remove('active');
+  this.view = 'map';
+  emitter.emit('view:changed', 'map');
+}
 
-  function toggleView() {
-    if (currentView === 'map') showText();
-    else showMap();
-  }
-
-  function getCurrentView() {
-    currentView = ( _.hasClass(mapView, 'active') ) ? 'map' : 'text';
-  }
-
-  module.exports.init = init;
-  module.exports.toggleView = toggleView;
-})();
+Switcher.prototype.toggleView = function() {
+  if (this.view === 'map') this.showText();
+  else this.showMap();
+}
