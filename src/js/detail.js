15328990b0f1483ecc357d9363068c086fbb9e5e
Roy Hewitt <roy_hewitt@fws.gov>
2017-02-17 09:17:02 -0500

    Convert to ES6 and contructor pattern

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,100 +1,85 @@
-(function () {
-  'use strict';
+const emitter = require('./mediator');
+const querystring = require('query-string');
+const _ = require('./util');
 
-  var emitter = require('./mediator');
-  var OfficeService = require('./offices');
-  var templates = {
-    init: require('../templates/detail-init.jade'),
-    detail: require('../templates/detail.jade'),
-    nearest: require('../templates/nearest.jade')
-  };
+const templates = {
+  init: require('../templates/detail-init.pug'),
+  detail: require('../templates/detail.pug'),
+  nearest: require('../templates/nearest.pug')
+};
 
-  var querystring = require('query-string');
-  var _ = require('./util');
+const Detail = function(data) {
+  this.offices = data.offices;
+  this.initialOffice = data.initialOffice;
+  this.active = data.initialView;
 
-  var opts = {}, active;
+  this.container = _.create('aside', 'detail-container', document.body);
+  this.close = _.create('button', 'detail-toggle', this.container);
+  this.close.innerHTML = '&#9650;';
+  this.content = _.create('section', 'detail-content', this.container);
 
-  function init() {
-    createDetail();
-    registerHandlers();
+  this.content.innerHTML = templates.init();
+  this.container.addEventListener('click', this.blur.bind(this));
+  this.close.addEventListener('click', this.toggle.bind(this));
+  this.content.addEventListener('click', delegatedOfficeLink.bind(this));
+  emitter.on('office:selected', this.renderOffice.bind(this));
+  emitter.on('marker:click', this.renderOffice.bind(this));
+  emitter.on('autocomplete:keyup', this.hide.bind(this));
+  emitter.on('view:changed', this.hide.bind(this));
+  emitter.on('zoom:fullextent', this.hide.bind(this));
+  emitter.on('found:office', this.renderOffice.bind(this));
+  emitter.on('offices:random', this.renderOffice.bind(this));
+  emitter.on('found:nearest', this.renderNearest.bind(this));
+
+  if (this.active) this.show();
+}
+
+module.exports = Detail;
+
+function delegatedOfficeLink (e) {
+  e.preventDefault();
+
+  if (e.target.nodeName === 'A'){
+    const officeName = querystring.parse(e.target.search).q.replace(/-/g, ' ');
+    const office = this.offices.search(officeName)[0];
+    this.renderOffice(office);
+    emitter.emit('office:selected', office);
   }
+}
 
-  function createDetail() {
-    opts.container = _.create('aside', 'detail-container', document.body);
-    opts.close = _.create('button', 'detail-toggle', opts.container);
-    opts.close.innerHTML = '&#9650;';
-    opts.content = _.create('section', 'detail-content', opts.container);
-    opts.content.innerHTML = templates.init();
-  }
+Detail.prototype.blur = function() {
+  emitter.emit('blur:input');
+}
 
-  function registerHandlers() {
-    opts.container.addEventListener('click', blurInput);
-    opts.close.addEventListener('click', toggleDetail);
-    opts.content.addEventListener('click', delegatedOfficeLink);
-    emitter.on('office:selected', renderOffice);
-    emitter.on('marker:click', renderOffice);
-    emitter.on('autocomplete:keyup', hideDetail);
-    emitter.on('found:office', renderOffice);
-    emitter.on('offices:random', renderOffice);
-    emitter.on('zoom:fullextent', hideDetail);
-    emitter.on('found:nearest', renderNearest);
-    emitter.on('view:changed', hideDetail);
-  }
+Detail.prototype.show = function() {
+  if (!this.active) emitter.emit('detail:show', -200);
+  this.active = true;
+  this.close.innerHTML = '&#9660;';
+  this.container.classList.add('active');
+}
 
-  function delegatedOfficeLink (e) {
-    e.preventDefault();
+Detail.prototype.hide = function() {
+  if (this.active) emitter.emit('detail:hide', 200);
+  this.active = false;
+  this.close.innerHTML = '&#9650;';
+  this.container.classList.remove('active');
+}
 
-    if (e.target.nodeName === 'A'){
-      var officeName = querystring.parse(e.target.search).q.replace(/-/g, ' ');
-      var office = OfficeService.getOffice(officeName);
-      renderOffice(office);
-      emitter.emit('office:selected', office);
-    }
-  }
+Detail.prototype.toggle = function() {
+  const eventName = (this.active) ? 'detail:hide' : 'detail:show';
+  const distance = (this.active) ? 200 : -200;
+  this.close.innerHTML = (this.active) ? '&#9650;' : '&#9660;';
+  this.container.classList.toggle('active');
+  this.active = !this.active;
+  emitter.emit(eventName, distance);
+}
 
-  function blurInput() {
-    emitter.emit('blur:input');
-  }
+Detail.prototype.renderOffice = function(office) {
+  this.content.innerHTML = templates.detail({ office: office.properties });
+  this.show();
+}
 
-  function showDetail() {
-    if (!active)
-      emitter.emit('detail:show', -200);
-    active = true;
-    opts.close.innerHTML = '&#9660;';
-    _.addClass(opts.container, 'active');
-  }
-
-  function hideDetail() {
-    if (active)
-      emitter.emit('detail:hide', 200);
-    active = false;
-    opts.close.innerHTML = '&#9650;';
-    _.removeClass(opts.container, 'active');
-  }
-
-  function toggleDetail() {
-    var eventName = (active) ? 'detail:hide' : 'detail:show';
-    var distance = (active) ? 200 : -200;
-    opts.close.innerHTML = (active) ? '&#9650;' : '&#9660;';
-    _.toggleClass(opts.container, 'active');
-    active = !active;
-    emitter.emit(eventName, distance);
-  }
-
-  function renderOffice(office) {
-    opts.content.innerHTML = templates.detail({ office: office.properties });
-    showDetail();
-  }
-
-  function renderNearest(nearest) {
-    opts.content.innerHTML = templates.nearest({ nearest: nearest });
-    showDetail();
-  }
-
-  exports.init = init;
-  exports.toggle = toggleDetail;
-  exports.show = showDetail;
-  exports.hide = hideDetail;
-  exports.renderOffice = renderOffice;
-
-})();
+Detail.prototype.renderNearest = function(nearest) {
+  this.content.innerHTML = templates.nearest({ nearest: nearest });
+  this.show();
+}
